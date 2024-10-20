4f8eea12395275bddf470a7a17af97d6d0fd53e4
donald <donald@dsimonet.fr>
2024-10-20 09:49:22 +0200

    update

--- tempSensor.cpp
+++ tempSensor.cpp
@@ -1,3 +1,26 @@
 #include "tempSensor.h"
 
 
+void tempSensor::updateSensor(void){
+
+    
+
+}
+
+float getCorrectedTemp(uint8_t pin, float serie_resistor,  const float coeff[]){
+
+    int Vo;
+    float logR1, NTC, T, Tc;
+
+    Vo = analogRead(pin);
+
+    if(Vo == 0 || Vo == 1023)
+        return -273.15f; 
+
+    NTC =  (Vo * serie_resistor) / (1023 - Vo);
+    logR1 = log(NTC);
+    T = (1.0 / (coeff[0] + coeff[1]*logR1 + coeff[2]*logR1*logR1*logR1));
+    Tc = T - 273.15;
+
+    return Tc;
+}
